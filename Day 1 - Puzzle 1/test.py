ad089189cf15f93431d538e48d72a2e8fdfd8f2d
Jared <72302164+moosecow1@users.noreply.github.com>
2025-12-01 23:33:03 +0000

    Delete Day 1 - Puzzle 1/test.py

--- Day 1 - Puzzle 1/test.py
+++
@@ -1,6 +0,0 @@
-from main import parseInstruction
-
-assert parseInstruction("L10") == (False, 10)
-assert parseInstruction("L1") == (False, 1)
-assert parseInstruction("R10") == (True, 10)
-assert parseInstruction("R1") == (True, 1)
